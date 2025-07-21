4951d19c27a4e99ee8d196d88cfc94e27cfc77f7
saini <A0098421@kiongroup.com>
2025-07-21 23:50:40 +0530

    added input

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #this is my first python program
 def greet(name):
 
+    name= input("Enter your name: ")
+
     return f"Hello, {name}"     
 
 if __name__ == "__main__":
-    print(greet("DevOps_World"))
+    print(greet("name"))
